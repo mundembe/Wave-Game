7689b2d5fde416dc0241aecc6e903e1dd622cc37
mundembe <61733352+mundembe@users.noreply.github.com>
2022-02-09 22:00:52 +0200

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,23 @@
+package com;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Player extends GameObject{
+
+    Random r = new Random();
+    public Player(int x, int y, ID id){
+        super(x,y,id);
+
+    }
+
+    public void tick() {
+        x += velX;
+        y += velY;
+    }
+
+    public void render(Graphics g) {
+        g.setColor(Color.blue);
+        g.fillRect(x, y,32,32);
+    }
+}
