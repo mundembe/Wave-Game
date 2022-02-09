a4bfb5111d522a4e390fabb5977661d139227d85
mundembe <61733352+mundembe@users.noreply.github.com>
2022-02-09 22:01:26 +0200

    Create Window.java

---
+++ Window.java
@@ -0,0 +1,21 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends Canvas {
+    public Window(int height, int width, String title, Game game){
+        JFrame frame = new JFrame(title);
+
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setLocationRelativeTo(null);
+        frame.add(game);
+        frame.setVisible(true);
+        game.start();
+
+    }
+}
